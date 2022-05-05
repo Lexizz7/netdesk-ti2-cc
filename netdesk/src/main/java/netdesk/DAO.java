b4418a830e9c5648b5ac2ee7ef5bec7c89c49b3f
Joao Vitor Almeida Araujo Belchior <72211720+joaovbelchi@users.noreply.github.com>
2022-05-05 17:35:46 -0300

    Add files via upload
    update do DAO com getAnuncio individual

--- netdesk/src/main/java/netdesk/DAO.java
+++ netdesk/src/main/java/netdesk/DAO.java
@@ -4,17 +4,17 @@ import java.sql.*;
 
 public class DAO {
 	private Connection conexao;
-	
+
 	public DAO() {
 		conexao = null;
 	}
-	
+
 	public boolean conectar() {
-		String driverName = "org.postgresql.Driver";                    
+		String driverName = "org.postgresql.Driver";
 		String serverName = "localhost";
 		String mydatabase = "netdesk";
 		int porta = 5432;
-		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + mydatabase;
 		String username = "postgres";
 		String password = "426513";
 		boolean status = false;
@@ -24,7 +24,7 @@ public class DAO {
 			conexao = DriverManager.getConnection(url, username, password);
 			status = (conexao == null);
 			System.out.println("Conexao efetuada com o postgres!");
-		} catch (ClassNotFoundException e) { 
+		} catch (ClassNotFoundException e) {
 			System.err.println("Conexao nao efetuada com o postgres -- Driver nao encontrado -- " + e.getMessage());
 		} catch (SQLException e) {
 			System.err.println("Conexao nao efetuada com o postgres -- " + e.getMessage());
@@ -32,10 +32,10 @@ public class DAO {
 
 		return status;
 	}
-	
+
 	public boolean close() {
 		boolean status = false;
-		
+
 		try {
 			conexao.close();
 			status = true;
@@ -44,134 +44,166 @@ public class DAO {
 		}
 		return status;
 	}
+
 	// ----------------------------------------------------------------------------------------------------------------------------------
 	public boolean inserirUsuario(Usuario usuario) {
 		boolean status = false;
-		try {  
+		try {
 			Statement st = conexao.createStatement();
-			st.executeUpdate("INSERT INTO usuario (cpf, nome, datanasc, username, senha, email, cidade, estado, pais, avaliacao) "
-					       + "VALUES (" + usuario.getCPF() + ", '" + usuario.getNome() + "', " + usuario.getDataNasc() + ", '" + usuario.getUsername() + "', '" + usuario.getSenha() + "', " + usuario.getEmail() + ", '" + usuario.getCidade() + "', '" + usuario.getEstado() + "', '" + usuario.getPais() + "', " + usuario.getAvaliacao() + ")");
+			st.executeUpdate(
+					"INSERT INTO usuario (cpf, nome, datanasc, username, senha, email, cidade, estado, pais, avaliacao) "
+							+ "VALUES (" + usuario.getCPF() + ", '" + usuario.getNome() + "', " + usuario.getDataNasc()
+							+ ", '" + usuario.getUsername() + "', '" + usuario.getSenha() + "', " + usuario.getEmail()
+							+ ", '" + usuario.getCidade() + "', '" + usuario.getEstado() + "', '" + usuario.getPais()
+							+ "', " + usuario.getAvaliacao() + ")");
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
 	}
-	
-	
+
 	public boolean atualizarUsuario(Usuario usuario, String cpf) {
 		boolean status = false;
-		try {  
+		try {
 			Statement st = conexao.createStatement();
-			String sql = "UPDATE usuario SET nome = '" + usuario.getNome() + "', datanasc = " + usuario.getDataNasc() + ", username = '" + usuario.getUsername() + "', senha = '" + usuario.getSenha() + "', email = " + usuario.getEmail() + " WHERE cpf = " + cpf;
+			String sql = "UPDATE usuario SET nome = '" + usuario.getNome() + "', datanasc = " + usuario.getDataNasc()
+					+ ", username = '" + usuario.getUsername() + "', senha = '" + usuario.getSenha() + "', email = "
+					+ usuario.getEmail() + " WHERE cpf = " + cpf;
 			st.executeUpdate(sql);
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
 	}
-	
+
 	public boolean excluirUsuario(String CPF) {
 		boolean status = false;
-		try {  
+		try {
 			Statement st = conexao.createStatement();
 			st.executeUpdate("DELETE FROM usuario WHERE cpf = " + CPF);
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
 	}
-	
-    public boolean usuarioExiste(String CPF) {
-    	boolean status = false;
-    	try {
-    		Statement st = conexao.createStatement();
-    		ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE cpf = " + CPF);
-    		status = rs.next();
-    		st.close();
-    	} catch (SQLException u) {
-    		throw new RuntimeException(u);
-    	}
-    	return status;
-    }
-    
-    public Usuario procurarUsuario(String CPF) {
-    	Usuario resposta = new Usuario();
-    	try {
-    		Statement st = conexao.createStatement();
-    		ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE cpf = " + CPF);
-    		if(rs.next()){
-	              resposta = new Usuario(
-                       rs.getString("cpf"),
-                       rs.getString("nome"),
-                       rs.getDate("datanasc"),
-                       rs.getString("username"),
-                       rs.getString("senha"),
-                       rs.getString("email"),
-                       rs.getString("cidade"),
-                       rs.getString("estado"),
-                       rs.getString("pais"),
-                       rs.getInt("avaliacao"));
-	          }
-    		st.close();
-    	} catch (SQLException u) {
-    		throw new RuntimeException(u);
-    	}
-    	return resposta;
-    }
-	
-    // ----------------------------------------------------------------------------------------------------------------------------------
+
+	public boolean usuarioExiste(String CPF) {
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE cpf = " + CPF);
+			status = rs.next();
+			st.close();
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public Usuario getUsuario(String CPF) {
+		Usuario resposta = new Usuario();
+		try {
+			Statement st = conexao.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE cpf = " + CPF);
+			if (rs.next()) {
+				resposta = new Usuario(
+						rs.getString("cpf"),
+						rs.getString("nome"),
+						rs.getDate("datanasc"),
+						rs.getString("username"),
+						rs.getString("senha"),
+						rs.getString("email"),
+						rs.getString("cidade"),
+						rs.getString("estado"),
+						rs.getString("pais"),
+						rs.getInt("avaliacao"));
+			}
+			st.close();
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return resposta;
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------------------------
 	public Anuncio[] getAllAnuncios() {
 		Anuncio[] anuncios = null;
-		
-		try {
-			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-			ResultSet rs = st.executeQuery("SELECT * FROM anuncio");		
-	         if(rs.next()){
-	             rs.last();
-	             anuncios = new Anuncio[rs.getRow()];
-	             rs.beforeFirst();
 
-	             for(int i = 0; rs.next(); i++) {
-	                anuncios[i] = new Anuncio(
-                        rs.getInt("id"),
-                        rs.getString("cpf"),
-                        rs.getString("titulo"),
-                        rs.getString("descricao"),
-                        rs.getString("valor"),
-                        rs.getString("cpu"),
-                        rs.getString("ram"),
-                        rs.getString("gpu"),
-                        rs.getString("so"),
-                        rs.getString("armazenamento"),
-                        rs.getString("pais"),
-                        rs.getString("cidade"),
-                        rs.getString("estado"),
-                        rs.getString("situacao")
-                    );
-	             }
-	          }
-	          st.close();
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = st.executeQuery("SELECT * FROM anuncio");
+			if (rs.next()) {
+				rs.last();
+				anuncios = new Anuncio[rs.getRow()];
+				rs.beforeFirst();
+
+				for (int i = 0; rs.next(); i++) {
+					anuncios[i] = new Anuncio(
+							rs.getInt("id"),
+							rs.getString("cpf"),
+							rs.getString("titulo"),
+							rs.getString("descricao"),
+							rs.getString("valor"),
+							rs.getString("cpu"),
+							rs.getString("ram"),
+							rs.getString("gpu"),
+							rs.getString("so"),
+							rs.getString("armazenamento"),
+							rs.getString("pais"),
+							rs.getString("cidade"),
+							rs.getString("estado"),
+							rs.getString("situacao"));
+				}
+			}
+			st.close();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 		return anuncios;
 	}
 
+	public Anuncio getAnuncio(int id) {
+		Statement st = conexao.createStatement();
+		String sql = "SELECT * FROM anuncio WHERE id = " + id;
+		ResultSet rs = st.executeQuery(sql);
+		Anuncio temp = new Anuncio(
+				rs.getInt("id"),
+				rs.getString("cpf"),
+				rs.getString("titulo"),
+				rs.getString("descricao"),
+				rs.getString("valor"),
+				rs.getString("cpu"),
+				rs.getString("ram"),
+				rs.getString("gpu"),
+				rs.getString("so"),
+				rs.getString("armazenamento"),
+				rs.getString("pais"),
+				rs.getString("cidade"),
+				rs.getString("estado"),
+				rs.getString("situacao"));
+		return temp;
+	}
+
 	public boolean inserirAnuncio(Anuncio anuncio) {
 		boolean status = false;
-		try {  
+		try {
 			Statement st = conexao.createStatement();
-			st.executeUpdate("INSERT INTO anuncio (id, cpf, titulo, descricao, valor, gpu, ram, cpu, so, armazenamento, pais, cidade, estado, situacao) "
-					       + "VALUES (" + anuncio.getID() + ", '" + anuncio.getCPF() + "', '" + anuncio.getTitulo() + "', '" + anuncio.getDescricao() + "', '" + anuncio.getValor() + "', '" + anuncio.getGPU() + "', '" + anuncio.getRAM() + "', '" + anuncio.getCPU() + "', '" + anuncio.getSO() + "', '" + anuncio.getArmazenamento() + "', '" + anuncio.getPais() + "', '" + anuncio.getCidade() + "', '" + anuncio.getEstado() + "', '" + anuncio.getSituacao() + "')");
+			st.executeUpdate(
+					"INSERT INTO anuncio (id, cpf, titulo, descricao, valor, gpu, ram, cpu, so, armazenamento, pais, cidade, estado, situacao) "
+							+ "VALUES (" + anuncio.getID() + ", '" + anuncio.getCPF() + "', '" + anuncio.getTitulo()
+							+ "', '" + anuncio.getDescricao() + "', '" + anuncio.getValor() + "', '" + anuncio.getGPU()
+							+ "', '" + anuncio.getRAM() + "', '" + anuncio.getCPU() + "', '" + anuncio.getSO() + "', '"
+							+ anuncio.getArmazenamento() + "', '" + anuncio.getPais() + "', '" + anuncio.getCidade()
+							+ "', '" + anuncio.getEstado() + "', '" + anuncio.getSituacao() + "')");
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
@@ -181,11 +213,16 @@ public class DAO {
 		boolean status = false;
 		try {
 			Statement st = conexao.createStatement();
-			String sql = "UPDATE anuncio SET titulo = '" + anuncio.getTitulo() + "', descricao = '" + anuncio.getDescricao() + "', valor = '" + anuncio.getValor() + "', gpu = '" + anuncio.getGPU() + "', ram = '" + anuncio.getRAM() + "', cpu = '" + anuncio.getCPU() + "', so = '" + anuncio.getSO() + "', armazenamento = '" + anuncio.getArmazenamento() + "', pais = '" + anuncio.getPais() + "', cidade = '" + anuncio.getCidade() + "', estado = '" + anuncio.getEstado() + "', situacao = '" + anuncio.getSituacao() + "' WHERE id = " + id + " AND cpf = " + cpf;
+			String sql = "UPDATE anuncio SET titulo = '" + anuncio.getTitulo() + "', descricao = '"
+					+ anuncio.getDescricao() + "', valor = '" + anuncio.getValor() + "', gpu = '" + anuncio.getGPU()
+					+ "', ram = '" + anuncio.getRAM() + "', cpu = '" + anuncio.getCPU() + "', so = '" + anuncio.getSO()
+					+ "', armazenamento = '" + anuncio.getArmazenamento() + "', pais = '" + anuncio.getPais()
+					+ "', cidade = '" + anuncio.getCidade() + "', estado = '" + anuncio.getEstado() + "', situacao = '"
+					+ anuncio.getSituacao() + "' WHERE id = " + id + " AND cpf = " + cpf;
 			st.executeUpdate(sql);
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
@@ -199,7 +236,7 @@ public class DAO {
 			st.executeUpdate(sql);
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
+		} catch (SQLException u) {
 			throw new RuntimeException(u);
 		}
 		return status;
